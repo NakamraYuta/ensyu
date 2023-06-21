8fc17c5cb498d16623989a76209f4450289a83ba
y-source-bit <2023java03-student002@la-study.com>
2023-06-21 17:29:46 +0900

    中村、本日最後のコミット

--- ensyu/src/main/java/la/dao/getProfileDAO.java
+++ ensyu/src/main/java/la/dao/getProfileDAO.java
@@ -18,6 +18,7 @@ public class getProfileDAO {
 	public ProfileBean getProfile(ProfileBean person) throws DAOException{
 		String sql = "select * from profile where email = ? and password =?";
 		
+		
 		try {
 			Class.forName("org.postgresql.Driver");
 		}catch (ClassNotFoundException e) {
@@ -25,21 +26,36 @@ public class getProfileDAO {
 			throw new DAOException("ドライバの登録に失敗しました");
 		}
 		
-		try(
-			Connection con= DriverManager.getConnection(url, user, pass);
-			PreparedStatement st = con.prepareStatement(sql);){
-			st.setString(1, person.getName());
-			st.setString(2, person.getPassword());
-			
-			try(ResultSet rs = st.executeQuery();){
-				
-				if (rs.next()){
-					int user_id = rs.getInt("user_id");
-					String name = rs.getString("name");
-					person.setUser_id(user_id);
-					person.setPassword(name);
-				}
-				return person;
+//		try(
+//			Connection con= DriverManager.getConnection(url, user, pass);
+//			PreparedStatement st = con.prepareStatement(sql);){
+//			
+//			String mail =	person.getEmail();
+//			String pass = person.getPassword();
+//			st.setString(1, mail);
+//			st.setString(2, pass);
+//			
+//			try(ResultSet rs = st.executeQuery();){
+//				
+//				if (rs.next()){
+//					int user_id = rs.getInt("user_id");
+//					String name = rs.getString("name");
+//					person.setUser_id(user_id);
+//					person.setPassword(name);
+//				}
+//				return person;
+		
+		
+		try (Connection connection = DriverManager.getConnection(url, user, pass);
+				 PreparedStatement statement = connection.prepareStatement(sql);) {
+				statement.setString(1, person.getEmail());
+				statement.setString(2, person.getPassword());
+				try (ResultSet resultSet = statement.executeQuery();) {
+					if (resultSet.next()) {
+						person.setUser_id(resultSet.getInt("user_id"));
+						person.setName(resultSet.getString("name"));
+						return person;
+					} 
 			}catch (SQLException e) {
 				e.printStackTrace();
 				throw new DAOException("レコードの取得に失敗しました。");
@@ -48,6 +64,7 @@ public class getProfileDAO {
 			e.printStackTrace();
 			throw new DAOException("レコードの取得に失敗しました。");
 		}
+		return person;
 	}
 	
 }
